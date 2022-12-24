73575a2a9b38cf1e8fd400e3f50033a24a11f22c
MaksFenek <makstreonin@gmail.com>
2022-12-24 19:02:55 +0100

    Add office model

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic'
 
+const Office = dynamic(() => import('@/components/canvas/Office'), { ssr: false })
 // Dynamic import is used to prevent a payload when the website starts, that includes threejs, r3f etc..
 // WARNING ! errors might get obfuscated by using dynamic import.
 // If something goes wrong go back to a static import to show the error.
@@ -12,7 +13,7 @@ export default function Page(props) {
 
 // Canvas components go here
 // It will receive same props as the Page component (from getStaticProps, etc.)
-Page.canvas = (props) => null
+Page.canvas = (props) => <Office scale={0.5} position-y={-1} />
 
 export async function getStaticProps() {
   return { props: { title: 'Index' } }

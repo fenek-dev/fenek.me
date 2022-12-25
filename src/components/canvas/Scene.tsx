9ec3c4da7c6b259fd20122c16e66a20a9d2cfc98
MaksFenek <makstreonin@gmail.com>
2022-12-25 01:46:49 +0100

    Add compressed images

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,15 +1,19 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload } from '@react-three/drei'
+import { OrbitControls, PerformanceMonitor, Preload, Stats } from '@react-three/drei'
+import { useState } from 'react'
 
 export default function Scene({ children, ...props }) {
+  const [dpr, setDpr] = useState(1.5)
   // Everything defined in here will persist between route changes, only children are swapped
   return (
-    <Canvas {...props}>
+    <Canvas {...props} dpr={dpr} frameloop='demand'>
+      <PerformanceMonitor onIncline={() => setDpr(2)} onDecline={() => setDpr(1)} />
       <directionalLight intensity={0.75} />
       <ambientLight intensity={0.75} />
       {children}
       <Preload all />
       <OrbitControls />
+      <Stats />
     </Canvas>
   )
 }

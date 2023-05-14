dc2588efa08d964ec0dab99d23cee61d7a7eb156
MaksFenek <makstreonin@gmail.com>
2023-05-14 12:52:43 +0200

    Upgrade to new versions

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,3 @@
-import { Header } from "./Header";
-import { Preview } from "./Preview";
-import { Projects } from "./Projects";
-import { Skills } from "./Skills";
-
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <Preview />
-      <Projects />
-      <Skills />
-    </>
-  );
+  return <main></main>;
 }

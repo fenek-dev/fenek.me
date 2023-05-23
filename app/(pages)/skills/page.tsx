bad0e00e2db09965211efb84a6a264d4258871c6
MaksFenek <makstreonin@gmail.com>
2023-05-23 23:22:25 +0300

    Add heart component

--- app/(pages)/skills/page.tsx
+++ app/(pages)/skills/page.tsx
@@ -1,3 +1,4 @@
+import { Heart } from "@/app/_modules/shared/atoms/Heart";
 import { SkillMap } from "@/app/_modules/shared/molecules/SkillMap";
 import React from "react";
 
@@ -5,6 +6,7 @@ const Skills = () => {
   return (
     <div className="min-h-screen">
       <SkillMap />
+      <Heart />
     </div>
   );
 };

d80b23219c6b74183b3da04135050a3822a0b589
MaksFenek <makstreonin@gmail.com>
2023-05-29 16:38:51 +0300

    Add yandex section

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -2,27 +2,14 @@ import { ProjectLine } from "@/app/_modules/shared/atoms/ProjectLine";
 import React from "react";
 import { AppInTheAirSection } from "./sections/AppInTheAirSection";
 import { ShopPartySection } from "./sections/ShopPartySection";
+import { YandexSection } from "./sections/YandexSection";
 
 const Projects = () => {
   return (
     <div className="absolute inset-0 snap-y snap-mandatory scroll-smooth overflow-y-auto overflow-x-hidden h-screen flex flex-col gap-32">
       <AppInTheAirSection />
       <ShopPartySection />
-
-      <ProjectLine
-        href="https://ya.ru"
-        technologies={[
-          "B2B",
-          "React",
-          "TypeScript",
-          "Redux",
-          "Sass",
-          "Hermione",
-        ]}
-        duration="Jun 2021 - Oct 2021 | 5 months"
-      >
-        Yandex
-      </ProjectLine>
+      <YandexSection />
     </div>
   );
 };

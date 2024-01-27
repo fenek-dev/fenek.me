8cd4b3e997a414d48f494a07d67cb6082f23605b
fenek-dev <makstreonin@gmail.com>
2024-01-27 10:57:36 +0300

    migrate to vite and three-react-fiber

--- app/(pages)/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { LazyMotion, domAnimation } from "framer-motion";
-import React from "react";
-
-const PagesLayout = ({ children }: { children: React.ReactNode }) => {
-  return <LazyMotion features={domAnimation}>{children}</LazyMotion>;
-};
-
-export default PagesLayout;
